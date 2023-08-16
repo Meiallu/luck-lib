9b891f66d976da673d133ac2883c456c63dd2095
Meiallu <daniloluigi.cavalcante@gmail.com>
2023-08-16 15:56:15 -0300

    Main cleanup

--- src/main/java/me/meiallu/Main.java
+++ src/main/java/me/meiallu/Main.java
@@ -5,34 +5,28 @@ import org.luck.system.nes.Instance;
 import org.luck.system.type.Animation;
 import org.luck.system.type.Object;
 import org.luck.system.type.Sprite;
-import org.luck.util.Audio;
-import org.luck.util.AudioManager;
 import org.luck.util.Keyboard;
 
 public class Main extends onTick {
-	Object Jogador;
+	Object Instance;
 
     public Main() {
 		new Instance("Bullet Hellween", 320, 180);
-		
-		Audio i = new Audio("audios/test.wav");
-		i.setVolume(-10);
-		AudioManager.play(i);
 
 		Animation idle = new Animation();
 		idle.setupFrames("images/run/", "run", ".png");
 		Sprite Player = new Sprite(idle);
 
 		Object Type = new Object(Player);
-		Jogador = Type.create(160, 90);
-		Type.setSpeed(12.0f);
+		Instance = Type.create(160, 90);
+		Instance.setSpeed(12.0f);
     }
 
 	@Override
 	public void everyTick() {
-		if ( Keyboard.isPressed('d') ) Jogador.setX( Jogador.getX() + 1 );
-		if ( Keyboard.isPressed('a') ) Jogador.setX( Jogador.getX() - 1 );
-		if ( Keyboard.isPressed('w') ) Jogador.setY( Jogador.getY() - 1 );
-		if ( Keyboard.isPressed('s') ) Jogador.setY( Jogador.getY() + 1 );
+		if ( Keyboard.isPressed('d') ) Instance.setX( Instance.getX() + 1 );
+		if ( Keyboard.isPressed('a') ) Instance.setX( Instance.getX() - 1 );
+		if ( Keyboard.isPressed('w') ) Instance.setY( Instance.getY() - 1 );
+		if ( Keyboard.isPressed('s') ) Instance.setY( Instance.getY() + 1 );
 	}
 }
