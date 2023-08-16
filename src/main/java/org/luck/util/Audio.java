4bd0402e756b5cc2273ec30d8569fcf6393185e2
Meiallu <daniloluigi.cavalcante@gmail.com>
2023-08-16 17:06:53 -0300

    bug fixes + Audio Class cleanup

--- src/main/java/org/luck/util/Audio.java
+++ src/main/java/org/luck/util/Audio.java
@@ -6,23 +6,26 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Audio {
-    private AudioInputStream str;
     private float vol = 0.0f;
-    private Clip c;
     private long time;
-
-    public Audio() {} 
+    private Clip clip;
+    private AudioInputStream str;
 
     public Audio(String path) {
         try {
-            str = AudioSystem.getAudioInputStream( new File(path).toURI().toURL() ); 
+            str = AudioSystem.getAudioInputStream( new File(path).toURI().toURL() );
+            clip = AudioSystem.getClip();
+            clip.open(str); 
         } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
         } catch (IOException e) { 
             e.printStackTrace(); 
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
         }
     }
 
@@ -31,26 +34,26 @@ public class Audio {
     public float getVolume() { return vol; }
     public void setVolume(float volume) { vol = volume; }
 
-    public Clip getClip() { return c; }
-    public void setClip(Clip clip) { c = clip; }
+    public Clip getClip() { return clip; }
+    public void setClip(Clip clip) { this.clip = clip; }
 
     public void pause() { 
-        time = c.getMicrosecondPosition();
-        c.stop();
+        time = clip.getMicrosecondPosition();
+        clip.stop();
     }
 
     public void resume() {
-        c.setMicrosecondPosition(time);
-        c.start();
+        clip.setMicrosecondPosition(time);
+        clip.start();
     }
 
     public void setSecond(float sec) {
-        c.setMicrosecondPosition( (long) sec * 1000000 );
+        clip.setMicrosecondPosition( (long) sec * 1000000 );
     }
 
-    public void loop(int count) { c.loop(count); }
+    public void loop(int count) { clip.loop(count); }
 
-    public boolean isOpen() { return c.isOpen(); }
-    public boolean isRunning() { return c.isRunning(); }
-    public boolean isActive() { return c.isActive(); }
+    public boolean isOpen() { return clip.isOpen(); }
+    public boolean isRunning() { return clip.isRunning(); }
+    public boolean isActive() { return clip.isActive(); }
 }
--- src/main/java/org/luck/util/AudioManager.java
+++ src/main/java/org/luck/util/AudioManager.java
@@ -1,25 +1,12 @@
 package org.luck.util;
 
-import java.io.IOException;
-
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
 
 public class AudioManager {
 
     public static void play(Audio a) { 
-        Clip clip = null;
-        
-        try {
-            clip = AudioSystem.getClip();
-            clip.open( a.getStream() );
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Clip clip = a.getClip();
 
         FloatControl gc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         gc.setValue( a.getVolume() );
