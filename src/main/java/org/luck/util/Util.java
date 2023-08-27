df8ce27835071f4011c5495c9d7d31d5967f70d8
Meiallu <daniloluigi.cavalcante@gmail.com>
2023-08-27 17:45:18 -0300

    Update Util.java

--- src/main/java/org/luck/util/Util.java
+++ src/main/java/org/luck/util/Util.java
@@ -1,15 +1,15 @@
 package org.luck.util;
-
 public class Util {
     
-    public static boolean isBetween(double num, double min, double max) {
-        if (num >= min && num <= max) {
-            return true; 
-        } else { return false; }
+    public static boolean isBetween(double i, double min, double max) {
+        if (i >= min && i <= max) { return true; } else { return false; }
     }
 
     public static boolean mod(double y, double z) { return (y % z == 0); }
     public static boolean mod(int y, int z) { return (y % z == 0); }
+
+    public static double round(double i) { return Math.round(i); }
+    public static float round(float i) { return Math.round(i); }
 
     public static float random(float min, float max) { 
         double inter = max - min + 1;
